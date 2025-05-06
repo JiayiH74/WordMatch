b176993b21b0dab9e043d7c7d1a8dffc52d92ef8
JiayiH74 <jiayih12@nycstudents.net>
2025-05-06 10:43:37 -0400

    Create WordMatch.java

---
+++ WordMatch.java
@@ -0,0 +1,17 @@
+public class WordMatch
+  {
+    private String secret;
+
+    public WordMatch(String Word)
+    {
+      secret = word;
+    }
+
+    public int scoreGuess(String guess)
+    {
+    }
+
+    public String findBetterGuess(String guess1, String guess2)
+    {
+    }
+  }
