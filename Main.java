44eedc193b70459c0002dcdca8a45f9a97b73aa1
JiayiH74 <jiayih12@nycstudents.net>
2025-05-06 10:59:22 -0400

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,19 @@
+public class WordMatch
+  {
+    private String secret;
+
+    public wordMatch(String word)
+    {
+      secret = word;
+    }
+
+    public int scoreGuess(String guess)
+    {
+      
+    }
+
+    public String findBetterGuess(String guess1, String guess2)
+    {
+      
+    }
+  }
